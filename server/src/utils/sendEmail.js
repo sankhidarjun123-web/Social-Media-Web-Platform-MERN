a5d14ca4dfb308baf01be20eca23c91bdd3cac95
Arjun Sankhi <SankhiArjun123@gmail.com>
2026-06-10 11:55:13 +0530

    that was trial last this is last last

--- server/src/utils/sendEmail.js
+++ server/src/utils/sendEmail.js
@@ -6,27 +6,10 @@ const dns = require("dns");
 dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
+    service: "gmail",
     auth: {
         user: process.env.EMAIL,
         pass: process.env.EMAIL_APP_PASSWORD
-    },
-    getSocket: (options, callback) => {
-        dns.resolve4("smtp.gmail.com", (err, addresses) => {
-            if (err) return callback(err);
-
-            const net = require("net");
-            const socket = net.connect({
-                host: addresses[0],
-                port: 587
-            });
-
-            callback(null, {
-                connection: socket
-            });
-        });
     }
 });
 
