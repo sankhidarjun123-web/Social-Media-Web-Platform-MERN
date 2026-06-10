8916e71571a067cbb768f69203b1602b5793b9b4
Arjun Sankhi <SankhiArjun123@gmail.com>
2026-06-10 09:31:45 +0530

    some changes with transporter in server

--- server/src/utils/sendEmail.js
+++ server/src/utils/sendEmail.js
@@ -4,10 +4,13 @@ const path = require("path");
 
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    family: 4,
     auth: {
         user: process.env.EMAIL,
-        pass: process.env.EMAIL_APP_PASSWORD
+        pass: process.env.APP_PASSWORD
     }
 });
 
